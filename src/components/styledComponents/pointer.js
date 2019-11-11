126c68e7c6599e32d5c181417825649c7b9ac1b2
salemm <s.e.jugr@gmail.com>
2019-11-11 12:57:02 -0600

    faq on status working

--- src/components/styledComponents/pointer.js
+++ src/components/styledComponents/pointer.js
@@ -10,7 +10,7 @@ const Pointer = styled.p`
     line-height: 17px;
     margin:3em;
     margin-left:8em;
-    text-transform: lowercase;
+    /* text-transform: lowercase; */
 `
 
 export default Pointer;
--- src/pages/preguntasfrecuentes.js
+++ src/pages/preguntasfrecuentes.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import styled from 'styled-components'
-import Pointer from '../components/styledComponents/pointer'
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styled from "styled-components"
+import Pointer from "../components/styledComponents/pointer"
 const TitleHeader = styled.p`
   height: 41px;
   color: #16a1b2;
@@ -11,28 +11,78 @@ const TitleHeader = styled.p`
   font-weight: 300;
   line-height: 41px;
   text-align: center;
-    text-align: center;
+  text-align: center;
 `
+const FaqWrapper = styled.div`
+    padding: 0 2em 2em 8em;
 
+`
+const FaqContainer = styled.div`
+    width: 50%;
+    height: auto;
+    box-sizing: border-box;
+    border: 1px solid rgba(176,176,176,0.5);
+`
 const Preguntas = () => {
+  return (
+    <Layout>
+      <SEO title="Preguntas frecuentes" />
+      <Pointer>Pregunta al experto</Pointer>
+      <div>
+        <TitleHeader>PREGUNTAS FRECUENTES</TitleHeader>
+      </div>
+      <div>
+        <div className="faq-title-dudas" />
+        <div className="faq-title-resueltas" />
+      </div>
 
-    return (
-        <Layout>
-            <SEO title="Preguntas frecuentes"/>
-            <Pointer>Pregunta al experto</Pointer>
-            <div>
-                <TitleHeader>PREGUNTAS FRECUENTES</TitleHeader>
-            </div>
-            <div>
-            <div className="faq-title-dudas"/>
-            <div className="faq-title-resueltas"/>
-            </div>
-           
-            <div>
-                <h1>hola preguntas frecuentes</h1>
-            </div>
-        </Layout>
-    )
+      <FaqWrapper>
+        <FaqContainer>
+          <ul uk-accordion="collapsible: false">
+            <li>
+              <a className="uk-accordion-title" href="#">
+                Item 1
+              </a>
+              <div className="uk-accordion-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </li>
+            <li className="uk-open">
+              <a className="uk-accordion-title" href="#">
+                Item 2
+              </a>
+              <div className="uk-accordion-content">
+                <p>
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor reprehenderit.
+                </p>
+              </div>
+            </li>
+            <li>
+              <a className="uk-accordion-title" href="#">
+                Item 3
+              </a>
+              <div className="uk-accordion-content">
+                <p>
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat proident.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </FaqContainer>
+
+        <div>
+          <img src="" alt="" />
+        </div>
+      </FaqWrapper>
+    </Layout>
+  )
 }
 
-export default Preguntas;
+export default Preguntas
